4c64e1e034c1cc8e3f4b82bd984cdb6e2ad40490
shkiper8591 <62032769+shkiper8591@users.noreply.github.com>
2026-02-19 18:17:52 +0300

    Новый rpyc киллер

--- Не должно попасть в релиз/rpyc_killer.py
+++ Не должно попасть в релиз/rpyc_killer.py
@@ -1,15 +1,20 @@
 import os
 import time
+
 if __name__ == "__main__":
     print("Запуск")
     time.sleep(2)
+
     try:
         path = str(os.path.realpath(__file__)).rsplit("\\", 2)[0] + "\Мы не отсюда\wnfh\scripts"
-        for i in os.listdir(path)[0:-1]:
-            for n in os.listdir(path+"/"+i):
-                if n.endswith(".rpyc"):
-                    os.remove(path + "/" + i + "/" +n)
+
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file.endswith(".rpyc"):
+                    os.remove(os.path.join(root, file))
+
     except Exception as e:
-        print("Ошибка "+str(e))
+        print("Ошибка " + str(e))
+
     print("Готово")
     time.sleep(1000)
